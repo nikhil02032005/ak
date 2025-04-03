52fccef1e244c093895903bfe127f356d3a63ff6
nikhil <laptop02035@gmail.com>
2025-04-03 23:11:45 +0530

    Updated website content

--- Annabelle/Annabelle Comes Home.js
+++ Annabelle/Annabelle Comes Home.js
@@ -22,7 +22,7 @@ function handleClick(page) {
     const pageContent = document.getElementById(`${page}-page`);
     if (page === 'Home') {
         if (clickCounts.Home === 1) {
-            window.open("https://www.example.com", "_blank");
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
         } else if (clickCounts.Home === 2) {
             window.open('../index.html', '_blank');
             clickCounts.Home = 0;  // Reset click count for Movies
@@ -31,7 +31,7 @@ function handleClick(page) {
     }
     if (page === 'Hollywood') {
         if (clickCounts.Hollywood === 1) {
-            window.open("https://www.example.com", "_blank");
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
         } else if (clickCounts.Hollywood === 2) {
             window.open('../hollywood.html', '_blank');
             clickCounts.Hollywood = 0;  // Reset click count for Movies
@@ -39,7 +39,7 @@ function handleClick(page) {
     }
     if (page === 'Bollywood') {
         if (clickCounts.Bollywood === 1) {
-            window.open("https://www.example.com", "_blank");
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
         } else if (clickCounts.Bollywood === 2) {
             window.open('../bollywood.html', '_blank');
             clickCounts.Bollywood = 0;  // Reset click count for Movies
@@ -47,7 +47,7 @@ function handleClick(page) {
     }
     if (page === 'Punjab') {
         if (clickCounts.Punjab === 1) {
-            window.open("https://www.example.com", "_blank");
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
         } else if (clickCounts.Punjab === 2) {
             window.open('../Punjab.html', '_blank');
             clickCounts.Punjab = 0;  // Reset click count for Movies
@@ -55,7 +55,7 @@ function handleClick(page) {
     }
     if (page === 'South') {
         if (clickCounts.South === 1) {
-            window.open("https://www.example.com", "_blank");
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
         } else if (clickCounts.South === 2) {
             window.open('../south.html', '_blank');
             clickCounts.South = 0;  // Reset click count for Movies
@@ -63,7 +63,7 @@ function handleClick(page) {
     }
     if (page === 'Web') {
         if (clickCounts.Web  === 1) {
-            window.open("https://www.example.com", "_blank");
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
         } else if (clickCounts.Web  === 2) {
             window.open('../web.html', '_blank');
             clickCounts.Web = 0;  // Reset click count for Movies
@@ -71,7 +71,7 @@ function handleClick(page) {
     }
     if (page === 'GENREs') {
         if (clickCounts.GENREs === 1) {
-            window.open("https://www.example.com", "_blank");
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
         } else if (clickCounts.GENREs === 2) {
             window.open('../movies.html', '_blank');
             clickCounts.GENREs = 0;  // Reset click count for Movies
@@ -79,7 +79,7 @@ function handleClick(page) {
     }
     if (page === 'More') {
         if (clickCounts.More === 1) {
-            window.open("https://www.example.com", "_blank");
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
         } else if (clickCounts.More === 2) {
             window.open('../movies.html', '_blank');
             clickCounts.More = 0;  // Reset click count for Movies
@@ -88,7 +88,7 @@ function handleClick(page) {
     
     if (page === 'sonic1') {
         if (clickCounts.sonic1 === 1) {
-            window.open("https://www.example.com", "_blank");
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
         } else if (clickCounts.sonic1 === 2) {
             window.open('Annabelle Comes Home1.html', '_blank');
             clickCounts. sonic1 = 0;  // Reset click count for Movies
@@ -96,7 +96,7 @@ function handleClick(page) {
     }
     if (page === 'sonic2') {
         if (clickCounts.sonic2 === 1) {
-            window.open("https://www.example.com", "_blank");
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
         } else if (clickCounts. sonic2 === 2) {
             window.open('Annabelle Comes Home2.html', '_blank');
             clickCounts. sonic2 = 0;  // Reset click count for Movies
@@ -104,7 +104,7 @@ function handleClick(page) {
     }
     if (page === 'sonic3') {
         if (clickCounts.sonic3 === 1) {
-            window.open("https://www.example.com", "_blank");
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
         } else if (clickCounts. sonic3 === 2) {
             window.open('Annabelle Comes Home3.html', '_blank');
             clickCounts. sonic3 = 0;  // Reset click count for Movies
@@ -115,7 +115,7 @@ function handleClick(page) {
 
     if (page === 'screen') {
         if (clickCounts.screen === 1) {
-            window.open("https://www.example.com", "_blank");
+            window.open("https://www.effectiveratecpm.com/wxmmscwyjt?key=3ad7df227f55ca82acc30e205189608b.com", "_blank");
         } else if (clickCounts. screen === 2) {
             window.open('movies.html', '_blank');
             clickCounts. screen = 0;  // Reset click count for Movies
